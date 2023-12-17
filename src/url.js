ca20932e427d24e05c6ca3fc4c44ed854c0a816b
Ercan SUALP <ercansualp@outlook.com.tr>
2023-12-17 21:52:56 +0300

    fsdfds

--- src/url.js
+++ src/url.js
@@ -1,4 +1,4 @@
-const backEndUrl = "http://localhost/instagram-clone-revised";
+const backEndUrl = "http://13.50.130.221:8080";
 const managerFolderUrl = backEndUrl + "/manager";
 
 export const postApi = managerFolderUrl + "/PostManager.php";
